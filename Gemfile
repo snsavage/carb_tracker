bf389ccaaed84ac80dbdf04b45e8e71e09309377
Scott Savage <snsavage@gmail.com>
2017-02-11 15:32:37 -0500

    Create nutrition_ix class for api calls

--- Gemfile
+++ Gemfile
@@ -35,11 +35,15 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'httparty'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
 
   gem 'rspec-rails', '~> 3.4'
+  gem 'dotenv-rails'
+  gem 'pry-rails'
 end
 
 group :test do
---
+++ app/models/nutrition_ix.rb
@@ -0,0 +1,102 @@
+class NutritionIx
+  include HTTParty
+
+  attr_reader :search, :line_delimited
+
+  ALLOWED_KEYS = [
+    :food_name,
+    :serving_qty,
+    :serving_unit,
+    :serving_weight_grams,
+    :nf_calories,
+    :nf_total_fat,
+    :nf_saturated_fat,
+    :nf_cholesterol,
+    :nf_sodium,
+    :nf_total_carbohydrate,
+    :nf_dietary_fiber,
+    :nf_sugars,
+    :nf_protein,
+    :nf_potassium,
+    :ndb_no,
+  ]
+
+  base_uri "https://trackapi.nutritionix.com"
+
+  def initialize(search = "", line_delimited = false)
+    @search = search
+    @line_delimited = line_delimited
+  end
+
+  def errors?
+    data[:message] ? true : false
+  end
+
+  def messages
+    data[:message] || "Your search was successful!"
+  end
+
+  def foods
+    return [] if errors?
+
+    @foods ||= parse_foods
+  end
+
+  def search=(search)
+    @search = search
+  end
+
+  def line_delimited=(line_delimited)
+    @line_delimited = line_delimited
+  end
+
+  def reload!
+    @data = call_api
+    @foods = nil
+  end
+
+  def data
+    @data ||= call_api
+  end
+
+  private
+  def parse_foods
+    @foods = data[:foods].collect do |food|
+      new_hash = {tag_id:  food[:tags][:tag_id]}
+      new_hash = self.class.filter_keys(food, ALLOWED_KEYS)
+
+      OpenStruct.new(new_hash)
+    end
+  end
+
+  def call_api
+    response = self.class.post(
+      "/v2/natural/nutrients", headers: headers, body: body
+    ).parsed_response
+
+    response.deep_symbolize_keys!
+  end
+
+  def headers
+    {
+      "Content-Type" => "application/json",
+      "x-app-id" => ENV["NUTRITION_IX_ID"],
+      "x-app-key" => ENV["NUTRITION_IX_APP"]
+    }
+  end
+
+  def body
+    {
+      :query => @search,
+      :line_delimited => @line_delimited
+    }.to_json
+  end
+
+  def self.filter_keys(hash, allow = [])
+    hash ||= {}
+
+    hash.keep_if do |key, value|
+      allow.include?(key)
+    end
+  end
+end
