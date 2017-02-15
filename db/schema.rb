b73255a66d4cc6159a5b7b60edc0f67171f92467
Scott Savage <snsavage@gmail.com>
2017-02-15 13:35:42 -0500

    Add User model from Devise generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215011403) do
+ActiveRecord::Schema.define(version: 20170215183256) do
 
   create_table "foods", force: :cascade do |t|
     t.string   "food_name"
@@ -49,6 +49,23 @@ ActiveRecord::Schema.define(version: 20170215011403) do
     t.datetime "updated_at",               null: false
     t.index ["food_id", "recipe_id"], name: "index_recipes_foods_on_food_id_and_recipe_id"
     t.index ["recipe_id", "food_id"], name: "index_recipes_foods_on_recipe_id_and_food_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
