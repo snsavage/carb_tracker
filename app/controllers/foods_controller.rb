3346fecfd2bce452cb38e9b9ded6e2873a93c538
Scott Savage <snsavage@gmail.com>
2017-02-22 12:11:08 -0500

    Remove unused code and spec from food_controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,15 +1,3 @@
 class FoodsController < ApplicationController
-  def new
-    @foods = Food.all
-  end
-
-  def search
-    if params[:search].present?
-      line_delimited = line_delimited == "true" ? true : false
-      Food.search_form(params[:search], line_delimited)
-    end
-
-    redirect_to new_food_path
-  end
 end
 
