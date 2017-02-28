f0dcd3dae02c0e90a55ecdc37ba89fc3e3b6ab9e
Scott Savage <snsavage@gmail.com>
2017-02-28 10:43:13 -0500

    Seed file setup and rake task to clean out db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.create(email: "snsavage@gmail.com", password: "password")
+User.create(email: "example@example.com", password: "password")
+
+kalua_pig = <<-RECIPE
+3 slices of bacon
+5 pound bone-in pork shoulder roast
+5 peeled garlic cloves
+1.5 tablespoons of Alaea Red Hawaiian Coarse Sea Salt
+1 cup water
+1 small cabbage, cored, and cut into 6 wedges
+RECIPE
+
+chile_lime_chicken_wings = <<-RECIPE
+0.5 medium onion, roughly chopped
+2 jalapeno peppers (or 1 serrano pepper), ribs and seeds removed
+3 garlic cloves, peeled
+0.5 cup cilantro, tightly packed
+Freshly ground pepper
+Zest from 2 limes
+0.25 cup lime juice
+2 tablespoons Paleo-friendly fish sauce (Red Boat!)
+2 tablespoons coconut aminos
+6 pounds chicken wings and/or drumsticks
+2 tablespoons melted fat of choice
+4 limes, cut into wedges
+RECIPE
+
+recipe_1 = Recipe.create(name: "Kalua Pig", serving_size: 8)
+foods_1 = NutritionIx.new(kalua_pig).foods
+recipe_1.foods << Food.find_or_create_from_api(foods_1)
+recipe_1.save
+
+recipe_2 = Recipe.create(name: "Chile Lime Chicken Wings", serving_size: 8)
+foods_2 = NutritionIx.new(chile_lime_chicken_wings).foods
+recipe_2.foods << Food.find_or_create_from_api(foods_2)
+recipe_2.save
+
+
+
---
+++ lib/tasks/db_clean.rake
@@ -0,0 +1,16 @@
+namespace :db do
+  desc "Remove all data from all db models"
+  task :clean => :environment do |env|
+    if Rails.env == "development"
+      User.delete_all
+      Log.delete_all
+      Entry.delete_all
+      Recipe.delete_all
+      Ingredient.delete_all
+      Food.delete_all
+    else
+      puts "DB Clear: *** DO NOT RUN THIS IN PRODUCTION*** "
+    end
+  end
+end
+
