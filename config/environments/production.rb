4aa5a7abde403b94144f41896d5fd2585e9b4941
Scott Savage <snsavage@gmail.com>
2017-03-27 15:21:25 -0400

    Update host setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => "carbtracker.herokuapp.com" }
+
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
