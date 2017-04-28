1461eaf2b3b542c33a1db8b70d5454df168b3862
Scott Savage <snsavage@gmail.com>
2017-04-28 10:39:54 -0400

    Add basic Ajax handling for food api search

--- app/assets/javascripts/foods.js
+++ app/assets/javascripts/foods.js
@@ -1,4 +1,36 @@
 $(function () {
+  $('#foods-search-form input[type=submit]').on('click', function(event) {
+    var $searchBox = $('#foods-search-form #recipe_search');
+    var url = "/foods/search";
+
+    $('#foods-search-form .foods-search-flash').remove();
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    $.getJSON(url, {query: $searchBox.val()})
+      .done(function(data) {
+        $searchBox.val("");
+      })
+      .fail(function(data) {
+        var error = data.responseJSON['error'];
+
+        result = '<div class="foods-search-flash flash-notice">';
+        result += '<span>' + error + '</span>';
+        result += '</div>';
+
+        $('#foods-search-form').prepend(result);
+      });
+    // Call the api
+    // On success add to ingredients list
+    // Remove search terms
+    // On failure add message
+
+  });
+
+});
+
+$(function () {
   $('.foods-sort').on('click', function(event) {
     var url = event.target.pathname + event.target.search;
 
